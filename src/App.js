d2f35506b9528c7d5ad63b105c80beecec12c5ea
Amanda Stern <amanda.stern.73@gmail.com>
2023-03-27 16:42:50 -0400

    stub up palette component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Palette from './Palette.js'
+import seedColors from './seedColors';
 
 function App() {
   return (
     <div className="App">
-      
+      <Palette {...seedColors} />
     </div>
   );
 }
---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+const Palette = () => {
+  return ( 
+    <>
+      <div className="Palette">
+        <div className="Palette-colors"></div>
+      </div>
+    </>
+  );
+}
+
+export default Palette;
