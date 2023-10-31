8c7a8067a46bf7414947f69a04c88602d0f6baef
Amanda Stern <amanda.stern.73@gmail.com>
2023-10-31 13:10:09 -0400

    update styling for navbar

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,18 +1,26 @@
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
+import './NavBar.css'
 
 const NavBar = ({ sliderLevel, handleSliderChange }) => {
 
   return ( 
     <>
-      <header className="header">
+      <header className="NavBar">
         <div className="logo">
           <a href="#">reactcolorpicker</a>
         </div>
-        <div className="slider">
-          <Slider defaultValue={sliderLevel} min={100} max={900} step={100}
-            onChange={handleSliderChange}
-          />
+        <div className="slider-container">
+          <span>Level: {sliderLevel} </span>
+          <div className="slider">
+            <Slider 
+              defaultValue={sliderLevel} 
+              min={100} 
+              max={900} 
+              step={100}
+              onChange={handleSliderChange}
+            />
+          </div>
         </div>
       </header>
     </>
