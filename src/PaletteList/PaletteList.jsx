5cb476cb03632b7e2f2c748894e53f694cb28f00
Amanda Stern <amanda.stern.73@gmail.com>
2023-11-12 20:58:12 -0500

    start styling paletteList component

--- src/PaletteList/PaletteList.jsx
+++ src/PaletteList/PaletteList.jsx
@@ -1,17 +1,56 @@
 import MiniPalette from "../MiniPalette/MiniPalette";
+import { styled } from '@mui/material/styles'
 import { v4 as uuidv4 } from 'uuid'
 
-const PaletteList = ({ palettes }) => {
+const RootDiv = styled('div')(() => ({
+  backgroundColor: 'blue',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+}))
 
+const ContainerDiv = styled('div')(() => ({
+  width: '50%',
+  display: 'flex',
+  alignItems: 'flex-start',
+  flexDirection: 'column',
+  flexWrap: 'wrap',
+  border: '2px solid white'
+}))
+
+const Nav = styled('nav')(() => ({
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'space-between',
+  color: 'white',
+}))
+
+const PalettesDiv = styled('div')(() => ({
+  boxSizing: 'border-box',
+  width: '100%',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 30%)',
+  gridGap: '5%'
+}))
+
+const PaletteList = ({ palettes }) => {
   return ( 
     <>
-      <div>
-        <MiniPalette />
-        <h1>React Colors</h1>
+      <RootDiv>
+        <ContainerDiv>
+
+        <Nav><h1>React Colors</h1></Nav>
+
+        <PalettesDiv>
+          <MiniPalette />
           {palettes.map(palette => (
             <MiniPalette {...palette} key={uuidv4()} />
           ))}
-      </div>
+        </PalettesDiv>
+
+        </ContainerDiv>
+      </RootDiv>
     </>
   );
 }
