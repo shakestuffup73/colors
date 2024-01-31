fc1b48acf680f44a8965f633cf39635c23bf5e4c
Amanda Stern <amanda.stern.73@gmail.com>
2024-01-30 21:31:29 -0500

    add color picker to drawer, set state for current color, add buttons, create custom button color themes

--- src/NewPaletteForm/NewPaletteForm.jsx
+++ src/NewPaletteForm/NewPaletteForm.jsx
@@ -1,26 +1,51 @@
-import { styled, useTheme } from '@mui/material/styles';
+import { styled, useTheme, ThemeProvider, createTheme, alpha, getContrastRatio } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-
+import { ChromePicker } from 'react-color';
 import { useState } from 'react';
 
 const drawerWidth = 240;
+
+const greenBase = '#354f52';
+const greenMain = alpha(greenBase, 0.7);
+
+const blueBase = '#6488a4';
+const blueMain = alpha(blueBase, 0.7);
+
+const cherryBase = '#66202c';
+const cherryMain = alpha(cherryBase, 0.7);
+
+const theme = createTheme({
+  palette: {
+    green: {
+      main: greenMain,
+      light: alpha(greenBase, 0.5),
+      dark: alpha(greenBase, 0.9),
+      contrastText: getContrastRatio(greenMain, '#fff') > 4.5 ? '#fff' : '#111',
+    },
+    blue: {
+      main: blueMain,
+      light: alpha(blueBase, 0.5),
+      dark: alpha(blueBase, 0.9),
+      contrastText: getContrastRatio(blueMain, '#fff') > 4.5 ? '#fff' : '#111',
+    },
+    cherry: {
+      main: cherryMain,
+      light: alpha(cherryBase, 0.5),
+      dark: alpha(cherryBase, 0.9),
+      contrastText: getContrastRatio(cherryMain, '#fff') > 4.5 ? '#fff' : '#111',
+    }
+  },
+});
 
 const Main = styled('main', {
   shouldForwardProp: (prop) => prop !== 'open',
@@ -68,7 +93,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 }));
 
 const NewPaletteForm = () => {
-  const theme = useTheme();
+  //const theme = useTheme();
   const [open, setOpen] = useState(false);
 
   const handleDrawerOpen = () => {
@@ -79,7 +104,10 @@ const NewPaletteForm = () => {
     setOpen(false);
   };
 
+  const [currentColor, setCurrentColor] = useState('teal');
+
   return (
+    <ThemeProvider theme={theme}>
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
@@ -113,67 +141,23 @@ const NewPaletteForm = () => {
       >
         <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            <ChevronLeftIcon />
           </IconButton>
         </DrawerHeader>
         <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+        <Typography variant="h5">Design Your Palette</Typography>
+        <div>
+          <Button variant="contained" color="secondary">Clear Palette</Button>
+          <Button variant="contained" color="primary">Random Color</Button>
+        </div>
+        <ChromePicker color={currentColor} onChangeComplete={(newColor) => console.log(newColor)}/>
+        <Button variant="contained" color='cherry'>Add Color</Button>
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
       </Main>
     </Box>
+    </ThemeProvider>
   );
 }
 
